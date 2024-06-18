2972007b0b5c1f7d4783711ad1e68f713aa101d5
Amr Alaaeldin <amralaaeldin.dev@gmail.com>
2024-06-18 20:00:39 +0300

    Initial commit.

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+const SERVER_PORT = 3000;
+
+app.listen(SERVER_PORT, () => {
+  console.log(`Server is running on port ${SERVER_PORT}`);
+});
